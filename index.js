791adef08c35c6ef7a4eddd3a5fc0601caf8717b
Encrypto27 <diegoallies27@gmail.com>
2022-01-04 16:02:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+fetch("https://fakestoreapi.com/products")
+.then((response) => response.json())
+.then((data)=> {
+    console.log(data);
+    data.forEach(result =>
+        {document.getElementById("results").innerHTML += `
+     
+          <div class="card">
+            <img src="${result.image}" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title fs-6 ">${result.title}</h5>
+              <p class="fs-6">${result.description}</p>
+              <p class="fs-6"> price: R${result.price}</p>
+            </div>
+           
+          </div>
+      
+      
+     
+
+        `});
+});
